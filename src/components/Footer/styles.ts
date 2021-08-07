76924b288fe2752583923f38552eee68ef228378
Isaque Souza <isaquetoonix245@gmail.com>
2021-08-07 02:46:56 -0300

    [update](style): [created base of Footer component]

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 30vh;
+
+  background:
+    linear-gradient(
+      205deg,rgba(14, 8, 35, 0.4) 0%,
+      rgba(60, 31, 139, 0.4) 100%
+    );
+
+  box-shadow: rgb(255 255 255 / 20%) 0px 0.5px 0px 0px inset;
+`;
